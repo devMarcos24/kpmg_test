c1823297f8e44cf4a6d568a1a7fdb44e23d152bc
Marcos Taylor <dev.marcos1995@gmail.com>
2024-02-21 18:38:53 -0300

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
+
 export default defineConfig({
+  base: "/kpmg_test",
   plugins: [react()],
   resolve: {
     alias: {
